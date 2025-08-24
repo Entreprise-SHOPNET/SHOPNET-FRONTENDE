4856c814432a7b19571ed4651aefa0397072ebcb
Entreprise SHOPNET <Entrepriseshopia@gmail.com>
2025-08-24 19:28:10 +0100

    Fix Babel config for Expo Router

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,16 @@
 
 module.exports = function(api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo', 
+      '@babel/preset-typescript'
+    ],
     plugins: [
-      'react-native-reanimated/plugin', // obligatoire pour Reanimated 3.x
+      // Plugins recommandés pour expo-router
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
     ],
   };
 };
